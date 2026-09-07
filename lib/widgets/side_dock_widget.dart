3f80b479982fa7a655a687cc12f8c3f113615204
Cody Weisenberger <codyweisenberger@Codys-MacBook-Pro.local>
2026-09-07 16:42:40 -0600

    fix

--- lib/widgets/side_dock_widget.dart
+++ lib/widgets/side_dock_widget.dart
@@ -67,7 +67,7 @@ class SideDockFloating extends StatelessWidget {
           child: _FloatingSideButton(
             theme: theme,
             size: 80,
-            label: 'Survival',
+            label: 'Battle',
             assetPath: 'assets/images/ui/trialsicon.png',
             onTap: onBattle,
           ),
